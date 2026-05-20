7b9436b08bd0f5aad13878a15df6b96af441f235
bjustin07 <baquiranjustin07@gmail.com>
2026-05-20 23:53:27 +0800

    feature:canvas painting.

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const gridContainer = document.querySelector(".grid");
 const rowCount = 16;
 const columnCount = 16;
 for(let i = 0; i < rowCount; i++){
-    const pixelRowContainer = document.createElement("div");
-    pixelRowContainer.style.border = "2px solid blue";
-    pixelRowContainer.style.display  = "flex";
-    pixelRowContainer.style.flexDirection = "column";
-    pixelRowContainer.style.gap = "5px";
-    gridContainer.append(pixelRowContainer);
+    const pixelColumnContainer = document.createElement("div");
+    // pixelColumnContainer.style.border = "1px solid black"; for debugging
+    pixelColumnContainer.style.flexDirection = "column";
+    pixelColumnContainer.style.display = "flex";
+    pixelColumnContainer.style.flex = "1";
+    gridContainer.append(pixelColumnContainer);
     for(let j = 0; j < columnCount; j++){
         const pixelContainer = document.createElement("div");
-        pixelContainer.style.border = "2px solid red";
-        pixelContainer.style.display  = "flex";
-        pixelContainer.style.gap = "5px";
-        pixelContainer.addEventListener("")
-        pixelRowContainer.append(pixelContainer);
+            // pixelContainer.style.border = "1px solid red";  for debugging
+        pixelContainer.style.flex = "1";
+        pixelContainer.style.minWidth = "1px";
+        pixelContainer.style.minHeight = "1px";
+        pixelContainer.addEventListener("mouseenter", (e)=>{
+            e.target.style.backgroundColor = "green";
+        })
+        pixelColumnContainer.append(pixelContainer);
     }
 }
+
+//todo, add buttons to reset canvas and to resize "painting" width
+//by dynamically adjusting row/Column count
